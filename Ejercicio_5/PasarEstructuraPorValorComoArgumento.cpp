bf8a2836acd89f5690740972b56a5dc65933cdb6
GabriHR <ghernrod@myuax.com>
2023-12-12 17:49:24 +0100

    Función main para comprobar que funciona todas las estructuras definidas anteriormente.

--- Ejercicio_5/PasarEstructuraPorValorComoArgumento.cpp
+++ Ejercicio_5/PasarEstructuraPorValorComoArgumento.cpp
@@ -11,3 +11,9 @@ void imprimirEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << ", edad: " << estudiante.edad << ", promedio: " << estudiante.promedio << endl;
 }
 
+int main() {
+    Estudiante estudiante1 = {"Gabriel", 19, 67.5};
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
