24ae7503948a22f7f987910e53b97b025c4da9ec
GabriHR <ghernrod@myuax.com>
2023-12-12 17:37:55 +0100

    Declaración de funcion prinipal para probar que funciona todo lo definido anteriormente.

--- Ejercicio_4/CreacionAliasTipoEstructuras.cpp
+++ Ejercicio_4/CreacionAliasTipoEstructuras.cpp
@@ -16,3 +16,14 @@ EstudianteAlias copiarEstudiante(const EstudianteAlias& original) {
 
     return copia;
 }
+
+int main() {
+    EstudianteAlias estudianteOriginal = {"Gabriel", 4, 70.0};
+    EstudianteAlias estudianteCopia = copiarEstudiante(estudianteOriginal);
+    cout << "Estudiante Original:\n";
+    cout << "Nombre: " << estudianteOriginal.nombre << ", edad: " << estudianteOriginal.edad << ", promedio: " << estudianteOriginal.promedio << endl;
+    cout << "\nEstudiante Copiado:\n";
+    cout << "Nombre: " << estudianteCopia.nombre << ", edad: " << estudianteCopia.edad << ", promedio: " << estudianteCopia.promedio << endl;
+
+    return 0;
+}
