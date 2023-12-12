f572338e0c2151ba7d80f8b9a4aacc19635a54c3
GabriHR <ghernrod@myuax.com>
2023-12-12 17:07:42 +0100

    Definicion de las estructuras necesarias.

---
+++ Ejercicio_3/CopiaEstructuras.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+
+struct Estudiante copiarEstudiante(struct Estudiante original) {
+    struct Estudiante copia;
+    strcpy(copia.nombre, original.nombre);
+    copia.edad = original.edad;
+    copia.promedio = original.promedio;
+
+    return copia;
+}
+
