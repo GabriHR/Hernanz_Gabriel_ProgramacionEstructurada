5a3aa6c5416bd3b88620d91ef10a925a779591e4
GabriHR <ghernrod@myuax.com>
2023-12-12 12:13:44 +0100

    Funcion main para poder imprimir lo definido anteriormente.

--- Ejercicio_2/Uniones.cpp
+++ Ejercicio_2/Uniones.cpp
@@ -5,3 +5,18 @@ union TipoDato {
     float flotante;
     char cadena[50];
 };
+
+int main() {
+    union TipoDato miUnion;
+
+    miUnion.entero = 4;
+    printf("Valor como entero: %d\n", miUnion.entero);
+
+    miUnion.flotante = 3.14;
+    printf("Valor como decimal: %.2f\n", miUnion.flotante);
+
+    union TipoDato miUnionCadena = {.cadena = "Hola, Mundo!"};
+    printf("Valor como cadena: %s\n", miUnionCadena.cadena);
+
+    return 0;
+}
