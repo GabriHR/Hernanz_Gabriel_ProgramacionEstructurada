3b066a560cc1bf7dd4f4a5d1b9310bca19076167
GabriHR <ghernrod@myuax.com>
2023-12-12 18:30:10 +0100

    Función principal para comprobar que compila el código escrito anteriormente.

--- Ejercicio_5/PasarEstructuraPorReferenciaComoArgumento.cpp
+++ Ejercicio_5/PasarEstructuraPorReferenciaComoArgumento.cpp
@@ -12,3 +12,9 @@ void imprimirEstudiante(Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << ", edad: " << estudiante.edad << ", promedio: " << estudiante.promedio << endl;
 }
 
+int main() {
+    Estudiante estudiante1 = {"Nerea", 17, 97.5};
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
