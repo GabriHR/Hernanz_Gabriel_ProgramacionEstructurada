68e44913c3c752fb2ca2f541df88f521050e724c
GabriHR <ghernrod@myuax.com>
2023-12-12 11:59:19 +0100

    Agregar funciones a las estructuras definidas anteriormente.

--- 1_Estructuras/Estructuras.cpp
+++ 1_Estructuras/Estructuras.cpp
@@ -14,4 +14,37 @@ struct Nodo {
     struct Nodo* siguiente;
 };
 
+void agregarEstudiante(struct Nodo** lista, struct Estudiante nuevoEstudiante) {
+    struct Nodo* nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
+    nuevoNodo->datos = nuevoEstudiante;
+    nuevoNodo->siguiente = *lista;
+    *lista = nuevoNodo;
+}
 
+void imprimirLista(struct Nodo* lista) {
+    while (lista != NULL) {
+        printf("Nombre: %s, edad: %d, promedio: %.2f\n", lista->datos.nombre, lista->datos.edad, lista->datos.promedio);
+        lista = lista->siguiente;
+    }
+}
+
+void eliminarEstudiante(struct Nodo** lista, char nombreEliminar[50]) {
+    struct Nodo* actual = *lista;
+    struct Nodo* anterior = NULL;
+
+    while (actual != NULL) {
+        if (strcmp(actual->datos.nombre, nombreEliminar) == 0) {
+            if (anterior == NULL) {
+                *lista = actual->siguiente;
+            } else {
+                anterior->siguiente = actual->siguiente;
+            }
+            free(actual);
+            return;
+        }
+        anterior = actual;
+        actual = actual->siguiente;
+    }
+
+    printf("Estudiante con nombre %s no encontrado.\n", nombreEliminar);
+}
