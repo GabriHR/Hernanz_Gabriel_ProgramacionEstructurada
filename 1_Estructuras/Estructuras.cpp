08bd037ada8870e6cb993eb8c43bbacccf00f827
GabriHR <ghernrod@myuax.com>
2023-12-12 12:04:34 +0100

    Definicion de la funcion main para imprimir lo definido anteriomente.

--- 1_Estructuras/Estructuras.cpp
+++ 1_Estructuras/Estructuras.cpp
@@ -48,3 +48,46 @@ void eliminarEstudiante(struct Nodo** lista, char nombreEliminar[50]) {
 
     printf("Estudiante con nombre %s no encontrado.\n", nombreEliminar);
 }
+
+int main() {
+    struct Estudiante estudiante1 = {"Gabriel", 18, 90.0};
+
+    struct Estudiante* estudiante2 = (struct Estudiante*)malloc(sizeof(struct Estudiante));
+    strcpy(estudiante2->nombre, "TheGrefg");
+    estudiante2->edad = 25;
+    estudiante2->promedio = 7.75;
+
+    struct Estudiante* estudiante3 = NULL;
+    estudiante3 = (struct Estudiante*)malloc(sizeof(struct Estudiante));
+    strcpy(estudiante3->nombre, "Nerea");
+    estudiante3->edad = 17;
+    estudiante3->promedio = 88.5;
+
+    struct Nodo* listaEstudiantes = NULL;
+
+    agregarEstudiante(&listaEstudiantes, estudiante1);
+    agregarEstudiante(&listaEstudiantes, *estudiante2);
+    agregarEstudiante(&listaEstudiantes, *estudiante3);
+
+    printf("Lista de estudiantes:\n");
+    imprimirLista(listaEstudiantes);
+
+    char nombreEliminar[50] = "Maria";
+    eliminarEstudiante(&listaEstudiantes, nombreEliminar);
+
+    printf("\nLista de estudiantes despues de eliminar a %s:\n", nombreEliminar);
+    imprimirLista(listaEstudiantes);
+
+    free(estudiante2);
+    free(estudiante3);
+
+    struct Nodo* actual = listaEstudiantes;
+    struct Nodo* siguiente = NULL;
+    while (actual != NULL) {
+        siguiente = actual->siguiente;
+        free(actual);
+        actual = siguiente;
+    }
+
+    return 0;
+}
