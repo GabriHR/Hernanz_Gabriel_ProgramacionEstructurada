b71a36c8cecbd2158fd39b7ac6b16752643e1e4a
GabriHR <ghernrod@myuax.com>
2023-12-12 18:21:03 +0100

    Definición de la estructura Estudiante con la utilización de una función void para almacenar los valores a dar a la estructura.

--- Ejercicio_5/PasarEstructuraPorValorComoArgumento.cpp
+++ Ejercicio_5/PasarEstructuraPorValorComoArgumento.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 struct Estudiante {
     string nombre;
     int edad;
@@ -11,9 +12,3 @@ void imprimirEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << ", edad: " << estudiante.edad << ", promedio: " << estudiante.promedio << endl;
 }
 
-int main() {
-    Estudiante estudiante1 = {"Gabriel", 19, 67.5};
-    imprimirEstudiante(estudiante1);
-
-    return 0;
-}
